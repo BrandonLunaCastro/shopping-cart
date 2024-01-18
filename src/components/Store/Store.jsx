ec7d18531cc6c3277965a1b5b0989c5f7d9d6605
Brandon luna <blunacastro2003@gmail.com>
2024-01-18 13:02:23 -0300

    feat: create component store

---
+++ src/components/Store/Store.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link, Outlet } from "react-router-dom";
+
+function Store() {
+  return (
+    <>
+      <div>Store</div>
+
+    </>
+  );
+}
+
+export default Store;
