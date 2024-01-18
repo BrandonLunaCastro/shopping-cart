c4885de3dc8cccced6f7852b41f8de11a61f2f43
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-18 18:23:57 -0300

    feat: create component navBar and import styles

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,42 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+function NavBar() {
+
+  const Nav = styled.nav`
+    background-color: aliceblue;
+    color: red;
+    display: flex;
+    justify-content: space-around;
+  `
+  const List = styled.nav`
+    display: inherit;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    & > li {
+      list-style-type: none;    
+    }
+  `
+  
+  return (
+    <>
+      <Nav>
+        <p>Shop</p>
+        <List>  
+          <li>
+            <NavLink  to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink   to="/store">Store</NavLink>
+          </li>
+        </List>
+      </Nav>
+      <div id="detail">
+          <Outlet></Outlet>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
