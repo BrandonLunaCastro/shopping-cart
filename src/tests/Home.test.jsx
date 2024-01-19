ee31633e0fade1681b76e7138eab96b46fa188bd
Brandon luna <blunacastro2003@gmail.com>
2024-01-19 12:57:55 -0300

    feat: test component home

---
+++ src/tests/Home.test.jsx
@@ -0,0 +1,17 @@
+import { getByRole, render, screen } from "@testing-library/react";
+import Home from "../components/Home";
+import { describe, expect, it } from "vitest";
+
+describe("Home component", () => {
+  it("I should correct render", () => {
+    render(<Home />);
+  });
+
+  it("It should by an image background" , () => {
+    render(<Home />);
+    const image = screen.getByTestId("bg-image")
+    expect(image).toBeInTheDocument();
+  })
+
+
+});
