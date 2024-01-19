296a539f0bfe68a30cb4f9479a5934c39d78dbca
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-19 18:41:31 -0300

    style: add components styled to Cards element

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+import fetchData from "../services/fetchData";
+import styled from "styled-components";
 
+const Cards = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill,minmax(250px, 1fr));
+  grid-template-rows: repeat(auto-fill,minmax(250px, 1fr));
+  gap: 20px;
+`
+
+const Card = styled.article`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid gray; 
+`
 function Store() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData().then(res => setData(res))
+  },[])
+
+  console.log(data)
   return (
     <>
-      <div>Store</div>
-
+      <h1>Main articles</h1>
+      <Cards>
+      { data && 
+        data.map((el) => {
+          return (
+            <Card key={el.id}>                                                                                   
+              <p>{el.title}</p>
+              <img src={el.image} alt={el.title}></img>
+              <button>Add do cart</button>
+            </Card>
+          )
+        })
+      }
+      </Cards>
     </>
   );
 }
