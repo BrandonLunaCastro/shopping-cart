ba37a60bf0b464dd1f6625c273ab8ad85d4391b5
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-19 18:40:32 -0300

    style: add styles to navBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,22 @@
 import { NavLink, Outlet } from "react-router-dom";
 import styled from "styled-components";
+import cart from "../assets/svg/cart-shopping.svg"
+
 
 // styles
   const Nav = styled.nav`
-    background-color: aliceblue;
+    background-color: #F4F4F2 ;
     color: red;
     display: flex;
     justify-content: space-around;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 3rem;
+    background:transparent; 
+    backdrop-filter: blur(1rem); 
+    border-bottom: 1px solid #E8E8E8;
   `
   const List = styled.nav`
     display: inherit;
@@ -18,24 +28,41 @@ import styled from "styled-components";
       text-decoration: none;
     }
   `
+  const Logo = styled.img`
+      width: 2.5rem;  
+      height: 2.5rem;
+      margin: auto 0 ;
+  `
+  const Detail = styled.section`
+      margin-top: 80px; 
+  `   
+
+  const Cart = styled.img`
+    width: 2rem;
+    height: 2rem;
+  `
+
 
 function NavBar() {
   return (
     <>
       <Nav>
-        <p>Shop</p>
+        <Logo src="./src/assets/images/logo.png" alt="logo" ></Logo>
         <List>  
           <li>
-            <NavLink  to="/">Home</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <NavLink   to="/store">Store</NavLink>
+            <NavLink to="/store">Store</NavLink>
+          </li>
+          <li>
+            <Cart src={cart} alt="cart-icon"></Cart>
           </li>
         </List>
       </Nav>
-      <div id="detail">
+      <Detail id="detail">
           <Outlet></Outlet>
-      </div>
+      </Detail>
     </>
   );
 }
