31f23970c78c1b17c97dc53a61e828acf6232fd0
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-20 18:23:32 -0300

    fix: refactor root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from "./router/Root";
 import  "./assets/reset.css"
+import App from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Root />
+    <App />
   </React.StrictMode>,
 )
