4e5c85759cec8365037ccf052430abe83a847a56
Brandon luna <blunacastro2003@gmail.com>
2024-01-22 11:46:02 -0300

    feat: add prop added to component Root

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [articles, setArticles] = useState(0)
   const [ added, setAdded ] = useState([]);
 
-  return <Root articles={articles} setArticles={setArticles} setAdded={setAdded}></Root>;
+  return <Root articles={articles} setArticles={setArticles} setAdded={setAdded} added={added}></Root>;
 
 }
 
