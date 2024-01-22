6b694e121e754c5b1fbd976e5920c581be9adf7e
Brandon luna <blunacastro2003@gmail.com>
2024-01-22 11:48:11 -0300

    feat: show elements on window cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -45,19 +45,42 @@ import { useState } from "react";
   `
 
   const Window = styled.article`
-    width: 500px;
-    height: 500px;
+    max-width: 500px;
+    max-height: 500px;
+    overflow-y: scroll;
     border: 1px solid gray;
     position: fixed;
     background-color: #333;
     color: white;
     right: 0;
     top: 3rem;
+    padding: 15px;
+  `
+  const ElementCart = styled.figure`
+    display: flex;
+    flex-direction: column;
+    & > div > img {
+      width: 10rem;
+      height: 10rem;
+
+    }
+    border: 1px solid white;
+    gap: 15px;
   `
 
-function NavBar({mount, added}) {
+  const AmountElements = styled.span`
+    background-color: #000;
+    padding: 5px;
+    color: white;
+    border-radius: 100%;
+    font-size: 1rem;
+    position: absolute;
+  `
+
+
+function NavBar({amount, added}) {
   const [ cartOpen, setCart ] = useState(false);
-  console.log(added)
+  
   const handleCart = () => {
     !cartOpen ? setCart(true) : setCart(false);
   }
@@ -75,14 +98,32 @@ function NavBar({mount, added}) {
           </li>
           <li>
             <Cart src={cart} alt="cart-icon" onClick={handleCart}></Cart>
-            <span>{mount}</span>
+            <AmountElements>{amount}</AmountElements>
           </li>
         </List>
       </Nav>
       { cartOpen && 
         (
           <Window>
-            <p>catntidad de articulos = {mount}</p>
+            {
+              added.lenght !== 0 && 
+              added.map(art => {
+                return (
+                  <ElementCart key={art.id}>
+                    <div>
+                      <img src={art.image}></img>
+                      <p>{art.title}</p>
+                      <p>{art.price}</p>
+                    </div>
+                    <div>
+                      <button>+</button>
+                      <button>-</button>
+                    </div>
+                  </ElementCart>
+                
+                )
+              })
+            }
             
           </Window>
         )
