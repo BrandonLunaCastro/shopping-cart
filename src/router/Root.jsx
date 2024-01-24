cade13e2cdffdac0cd347af57806b3bf509f6843
Brandon luna <blunacastro2003@gmail.com>
2024-01-24 11:00:42 -0300

    fix: format document

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,26 +1,51 @@
-import { createBrowserRouter as Router , RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter as Router,
+  RouterProvider,
+} from "react-router-dom";
 import NavBar from "../components/NavBar";
-import Home from "../components/Home"; 
+import Home from "../components/Home";
 import Store from "../components/Store";
 
-function Root({articles, setArticles, added ,setAdded}) {
+function Root({
+  articles,
+  setArticles,
+  added,
+  setAdded,
+  setData,
+  data,
+}) {
   const router = Router([
     {
-      path:"/",
-      element:<NavBar amount={articles} added={added}/>,
+      path: "/",
+      element: (
+        <NavBar
+          amount={articles}
+          added={added}
+          setAdded={setAdded}
+          data={data}
+        />
+      ),
       children: [
         {
-          path:"/",
-          element:<Home />
+          path: "/",
+          element: <Home />,
         },
         {
-          path:"/store",
-          element:<Store setArticles={setArticles} setAdded={setAdded} added={added}/>
-        }
-      ]
-    }
-  ])  
-  return <RouterProvider router={router}></RouterProvider> 
+          path: "/store",
+          element: (
+            <Store
+              setArticles={setArticles}
+              setAdded={setAdded}
+              added={added}
+              setData={setData}
+              data={data}
+            />
+          ),
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
-export default Root
+export default Root;
