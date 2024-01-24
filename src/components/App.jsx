2f813b55e40a38c6413699e30069f1ce28455d53
Brandon luna <blunacastro2003@gmail.com>
2024-01-24 11:25:59 -0300

    remove: delete state article

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,11 @@ import Root from "../router/Root";
 import { useState } from "react";
 
 function App() {
-  const [articles, setArticles] = useState(0);
   const [added, setAdded] = useState([]);
   const [data, setData] = useState([]);
 
   return (
     <Root
-      articles={articles}
-      setArticles={setArticles}
       setAdded={setAdded}
       added={added}
       setData={setData}
