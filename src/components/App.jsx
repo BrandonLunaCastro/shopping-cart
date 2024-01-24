c31238cf2b9acb843a864f0687ef21d6bfc53ee8
Brandon luna <blunacastro2003@gmail.com>
2024-01-24 10:58:26 -0300

    feat: format document and refactor states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,20 @@ import Root from "../router/Root";
 import { useState } from "react";
 
 function App() {
-  const [articles, setArticles] = useState(0)
-  const [ added, setAdded ] = useState([]);
+  const [articles, setArticles] = useState(0);
+  const [added, setAdded] = useState([]);
+  const [data, setData] = useState([]);
 
-  return <Root articles={articles} setArticles={setArticles} setAdded={setAdded} added={added}></Root>;
-
+  return (
+    <Root
+      articles={articles}
+      setArticles={setArticles}
+      setAdded={setAdded}
+      added={added}
+      setData={setData}
+      data={data}
+    ></Root>
+  );
 }
 
 export default App;
