3caee7b5880272163012126b72f102c4a595d760
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-29 08:42:06 -0300

    feat:import provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,12 @@
 import Root from "../router/Root";
-import { createContext, useState } from "react";
-import Home from "./Home";
-
-
-const mainContext = createContext({})
+import ShoppingProvider from "../context/ShoppingCartContext";
 
 function App() {
-  const [added, setAdded] = useState([]);
-  const [data, setData] = useState([]);
-
+  
   return (
-    <>
-      <Root>
-        <Home />
-      </Root>
-    </>
+    <ShoppingProvider>
+      <Root></Root>    
+    </ShoppingProvider>
   );
 }
-
 export default App;
