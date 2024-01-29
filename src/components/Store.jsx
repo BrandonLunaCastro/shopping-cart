817dddfb2a3e79cf1ab20b5df0d197af878c70d6
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-01-29 08:44:50 -0300

    fix: refactor function props
    BREAKING CHANGE: import shopping cart context

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import fetchData from "../services/fetchData";
 import styled from "styled-components";
+import { ShoppingCartContext } from "../context/ShoppingCartContext";
 
 const Cards = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-template-rows: repeat(auto-fill, minmax(250px, 1fr));
   gap: 20px;
-  align-items: center;
+  align-items: center;        
 `;
 
 const Card = styled.article`
@@ -29,19 +30,20 @@ const Card = styled.article`
     height: 45px;
   }
 `;
-function Store({ setArticles, setAdded, added, setData, data }) {
+function Store() {
   // const [data, setData] = useState([]);
+  //{ setArticles, setAdded, added, setData, data }
+  
+  const {setAdded, setData, added, data} = useContext(ShoppingCartContext);
 
   useEffect(() => {
     fetchData().then((res) => setData(res));
   }, [setData]);
 
-  // console.log(data)
-
-  const addToCart = (element) => {
-    console.log(element);
-    setAdded((prevState) => [...prevState, element]);
-  };
+  const addToCart = (id) => {
+    const selectElement = data.find((element) => element.id === id)
+    setAdded((prevState) => [...prevState, selectElement]);
+  };  
 
   return (
     <>
@@ -54,7 +56,7 @@ function Store({ setArticles, setAdded, added, setData, data }) {
                 <p>{el.title}</p>
                 <img src={el.image} alt={el.title}></img>
                 <span>${el.price}</span>
-                <button onClick={() => addToCart(el)}>Add to cart</button>
+                <button onClick={() => addToCart(el.id)}>Add to cart</button>
               </Card>
             );
           })}
@@ -64,3 +66,4 @@ function Store({ setArticles, setAdded, added, setData, data }) {
 }
 
 export default Store;
+  
