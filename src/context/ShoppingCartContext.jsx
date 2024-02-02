405f8456e1f77eb56ce2e38afbf66788a542fc63
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-02-02 09:40:44 -0300

    fix: add main functions to context

--- src/context/ShoppingCartContext.jsx
+++ src/context/ShoppingCartContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import getPrice from "../services/getPrice";
 
 export const ShoppingCartContext = createContext(null);
 
@@ -6,8 +7,48 @@ export default function ShoppingProvider ({children}) {
   const [added, setAdded] = useState([]);
   const [data, setData] = useState([]);  
 
+  const addArticle = (element) => setAdded((prevState) => [...prevState, element]);
+
+  const subTotal = added.reduce((acu, current) => {
+    return (acu += current.price);
+  }, 0);
+
+  const handleMore = (id) => {
+    const price = getPrice( data, id );
+    const newState = added.map((art) => {
+      if ( art.id === id ) {
+        return {...art, price: art.price + price, mount: art.mount + 1 }
+      }
+      return art
+    })
+    setAdded(newState);
+  } 
+
+  const handleReduce = (id) => {
+    const price = getPrice( data, id );
+    const actualElement = added.find(el => el.id === id )
+
+    if ( actualElement.price - price <= 0 ) {
+      const stateFiltered = added.filter((element) => element.id != id )
+      return setAdded(stateFiltered);
+    }
+    
+    const newState = added.map((art) => {
+      if ( art.id === id ) {
+        return {...art, price: art.price - price, mount: art.mount - 1 }
+      }
+      return art
+    })
+    setAdded(newState);
+  }
+
+  const handleDelete = (id) => {
+      const stateFilter = added.filter((art) => art.id !== id)
+      setAdded(stateFilter);
+  }
+
   return (
-      <ShoppingCartContext.Provider value={{added, setAdded, data, setData}}>
+      <ShoppingCartContext.Provider value={{added, subTotal, handleMore, handleReduce, handleDelete,data, setData, addArticle}}>
         {children}  
       </ShoppingCartContext.Provider>             
     )
