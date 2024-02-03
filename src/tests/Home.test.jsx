2f8c999845f3d4a3ef0d0d739a50ad0f59b17b62
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-02-03 10:09:43 -0300

    feat:  add beforeEach to render component

--- src/tests/Home.test.jsx
+++ src/tests/Home.test.jsx
@@ -1,13 +1,13 @@
 import { getByRole, render, screen } from "@testing-library/react";
 import Home from "../components/Home";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 
 describe("Home component", () => {
-  it("I should correct render", () => {
+  beforeEach(() => {
     render(<Home />);
-  });
+  })
+
   it("It should by an image background" , () => {
-    render(<Home />);
     const image = screen.getByTestId("bg-image")
     expect(image).toBeInTheDocument();
   })
