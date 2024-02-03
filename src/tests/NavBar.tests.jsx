7c1156e15b82813bd1190a289a62002b8ff6ff50
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-02-03 10:09:17 -0300

    feat: import dependencies

---
+++ src/tests/NavBar.tests.jsx
@@ -0,0 +1,9 @@
+import { describe,expect,expectTypeOf,it } from "vitest";
+import NavBar from "../components/NavBar";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ShoppingCartContext } from "../context/ShoppingCartContext";
+
+describe("render component NavBar", () => {
+
+});
