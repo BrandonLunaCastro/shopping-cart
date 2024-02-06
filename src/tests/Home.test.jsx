c5e20ea9d1c0b3287ce30e8599b5fa6a8a988487
BrandonLunaCastro <blunacastro2003@gmail.com>
2024-02-05 21:16:44 -0300

    fix: change name of test

--- src/tests/Home.test.jsx
+++ src/tests/Home.test.jsx
@@ -2,7 +2,7 @@ import { getByRole, render, screen } from "@testing-library/react";
 import Home from "../components/Home";
 import { beforeEach, describe, expect, it } from "vitest";
 
-describe("Home component", () => {
+describe("should be render first a Home", () => {
   beforeEach(() => {
     render(<Home />);
   })
@@ -11,4 +11,5 @@ describe("Home component", () => {
     const image = screen.getByTestId("bg-image")
     expect(image).toBeInTheDocument();
   })
+
 });
