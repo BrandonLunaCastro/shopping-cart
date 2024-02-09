fb4f6475343a85151b655be26729bf745168ae09
Brandon luna <blunacastro2003@gmail.com>
2024-02-09 12:55:45 -0300

    feat: refactor structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import { NavLink, Outlet } from "react-router-dom";
 import styled from "styled-components";
 import cart from "../assets/svg/cart-shopping.svg";
 import { useContext, useState } from "react";
-import getPrice from "../services/getPrice";
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
+import CartWindow from "./CartWindow";
 
 // styles
 const Nav = styled.nav`
@@ -45,29 +45,6 @@ const Cart = styled.img`
   cursor: pointer;
 `;
 
-const Window = styled.article`
-  max-width: 500px;
-  max-height: 500px;
-  overflow-y: scroll;
-  border: 1px solid gray;
-  position: fixed;
-  background-color: #333;
-  color: white;
-  right: 0;
-  top: 3rem;
-  padding: 15px;
-`;
-const ElementCart = styled.figure`
-  display: flex;
-  flex-direction: column;
-  & > div > img {
-    width: 10rem;
-    height: 10rem;
-  }
-  border: 1px solid white;
-  gap: 15px;
-`;
-
 const AmountElements = styled.span`
   background-color: #000;
   padding: 5px;
@@ -77,15 +54,8 @@ const AmountElements = styled.span`
   position: absolute;
 `;
 
-const Delete = styled.button`
-  background-color: crimson;
-  color: white;
-  width: 8rem;
-  height: 3rem;
-`
-
 function NavBar() {
-  const {added, handleMore, handleReduce, handleDelete, subTotal} = useContext(ShoppingCartContext);
+  const { added } = useContext(ShoppingCartContext);
   const [cartOpen, setCart] = useState(false);
   const handleCart = () => setCart(!cartOpen);
 
@@ -106,33 +76,7 @@ function NavBar() {
           </li>
         </List>
       </Nav>
-      {cartOpen && (
-        <Window>
-          <h2>Articles Added</h2>
-          {added.length !== 0 ? (
-            added.map((art) => {
-              return (
-                <ElementCart key={art.id} data-testid="article-cart" >
-                  <div>
-                    <img src={art.image}></img>
-                    <p>{art.title}</p>
-                    <p>${art.price.toFixed(2)}</p>
-                  </div> 
-                  <div>
-                    <button onClick={() => handleMore(art.id)}>+</button>
-                    <button onClick={() => handleReduce(art.id)}>-</button>
-                    <p>Amount: {art.mount} </p>
-                  </div>
-                  <Delete onClick={() => handleDelete(art.id)} >Remove Item</Delete>
-                </ElementCart>
-              );
-            })
-          ) : (
-            <p>The actual Cart is empty</p>
-          )}
-          {added.length !== 0 && <p>SubTotal: ${subTotal}</p>}
-        </Window>
-      )}
+      {cartOpen && <CartWindow />}
       <Detail id="detail">
         <Outlet></Outlet>
       </Detail>
