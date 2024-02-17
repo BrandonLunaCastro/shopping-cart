5b08b606810fbca436fcded808ac34b3c34cdb4b
brandon <blunacastro2003@gmail.com>
2024-02-17 13:06:45 -0300

    style: add styles to cart

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -7,29 +7,43 @@ import { verifyElement } from "../services/verifyElement";
 const Cards = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-template-rows: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(300px, 1fr));
   gap: 20px;
   align-items: center;
+  padding: 20px;
 `;
 
 const Card = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   border: 1px solid gray;
-  padding: 10px;
-  min-height: 250px;
+  padding-top: 10px;
+  height: 280px;
+  gap: 10px;
+  box-shadow: 0px 0px 5px #333;
+
   & > img {
-    width: 10rem;
-    height: 10rem;
-    object-fit: fill;
+    width: 8rem;
+    height: 8rem;
+    object-fit: contain;
   }
 
-  & > button {
-    width: 100%;
-    height: 45px;
+  .title {
+    text-align: center;
   }
+
+  .add__cart{
+    background-color: #15b00d;
+    color:  white;
+    border: none;
+    font-size: 1.2rem;
+    width: 100%;
+    height: 4rem;
+    cursor: pointer;
+  }
+
 `;
 function Store() {
   const { setData, added, data, addArticle } = useContext(ShoppingCartContext);
@@ -57,10 +71,10 @@ function Store() {
                 key={el.id}
                 data-testid="articles-container"
               >                                 
-                <p>{el.title}</p>
+                <p className="title"> {el.title}</p>
                 <img src={el.image} alt={el.title}></img>
                 <span>${el.price}</span>
-                <button onClick={() => addToCart(el.id)} name="add" data-testid={`btn-${el.id}`} >Add to cart</button>
+                <button className="add__cart" onClick={() => addToCart(el.id)} name="add" data-testid={`btn-${el.id}`} >Add to cart</button>
               </Card>
             );
           })}
