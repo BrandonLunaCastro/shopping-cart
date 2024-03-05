a5b4d2709bb6d3ceca078e0370a3efe209623f73
brandon <blunacastro2003@gmail.com>
2024-03-05 12:58:05 -0300

    fix: refactor component

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,10 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import fetchData from "../services/fetchData";
 import styled from "styled-components";
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
 import { verifyElement } from "../services/verifyElement";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import useData from "../hooks/useData";
+
+const Filter = styled.section`
+  
+`
 
 const Cards = styled.section`
   display: grid;
@@ -23,14 +28,15 @@ const Card = styled.article`
   border: 1px solid gray;
   padding-top: 10px;
   height: 280px;
+  color: #555;
   gap: 10px;
-  border-radius: 20px;
+  border-radius: 3px;
   overflow: hidden;
-  box-shadow: 0px 0px 5px #333;
+  
 
-  & > img {
-    width: 8rem;
-    height: 8rem;
+  & > a >img {
+    width: 7rem;
+    height: 7rem;
     object-fit: contain;
   }
 
@@ -38,46 +44,63 @@ const Card = styled.article`
     text-align: center;
   }
 
-  .add__cart{
-    background-color: #66BFBF;
-    color:  #ffff;
+  .add__cart {
+    background-color: #000000;
+    color: #ffff;
     border: none;
     font-size: 1.2rem;
     width: 100%;
     height: 4rem;
     cursor: pointer;
   }
-
 `;
 function Store() {
-  const { setData, added, data, addArticle } = useContext(ShoppingCartContext);
-  useEffect(() => {
-    fetchData().then((res) => setData(res));
-  }, [setData]);
+  const { added, addArticle } = useContext(ShoppingCartContext);
+  const [search, setSearch] = useState("")
 
+  const {data} = useData();
+
+  
   const addToCart = (id) => {
     const selectElement = data.find((element) => element.id === id);
     selectElement.amount = 1;
-    verifyElement(added, id)
-      ? alert("is exist")
-      : addArticle(selectElement);
+    verifyElement(added, id) ? alert("is exist") : addArticle(selectElement);
   };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    
+  }
 
   return (
     <>
-      <h1>Main articles</h1>
+      <Filter>
+        <label htmlFor="search">Search article</label>
+        <input type="search" name="search" id="search" onChange={handleChange} value={search}></input>
+        <label htmlFor="category">Category</label>
+        <select id="category">
+          <option value="all">All</option>
+          <option value=""></option>
+        </select>
+      </Filter>
       <Cards>
         {data &&
           data.map((el) => {
             return (
-              <Card 
-                key={el.id}
-                data-testid="articles-container"
-              >                 
-                <NavLink className="title" to={`${el.id}`} >{el.title}</NavLink>                
-                <img src={el.image} alt={el.title}></img>
+              <Card key={el.id} data-testid="articles-container">
+                <p className="title">{el.title}</p>
+                <Link to={`${el.id}`}>
+                  <img src={el.image} alt={el.title}></img>
+                </Link>
                 <span>${el.price}</span>
-                <button className="add__cart" onClick={() => addToCart(el.id)} name="add" data-testid={`btn-${el.id}`} >Add to cart</button>
+                <button
+                  className="add__cart"
+                  onClick={() => addToCart(el.id)}
+                  name="add"
+                  data-testid={`btn-${el.id}`}
+                >
+                  Add to cart
+                </button>
               </Card>
             );
           })}
